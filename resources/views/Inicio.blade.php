3f5349d50ceb965130e68d0ace7d6020e8f8b6e9
MassiSulic <massi.sulic@gmail.com>
2024-12-03 10:53:11 -0300

    Navbar finalizada en escritorio y avances en el home

--- resources/views/Inicio.blade.php
+++ resources/views/Inicio.blade.php
@@ -1,4 +1,48 @@
 <x-layout>
-    <h1 class=" text-4xl text-sans ">Pointer Club Español | Inicio</h1>
-    <p class=" text-sans ">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis natus totam ut nihil expedita laudantium officiis, similique cumque fugit rem illo exercitationem? Quasi aspernatur soluta molestias doloremque facere nisi blanditiis.</p>
+    <div class="w-full bg-neutral-100">
+        <!-- Sección Superior con Imagen y Texto -->
+        <div class="relative h-screen bg-cover bg-center flex justify-end items-center"
+            style="background-image: url({{ asset('image/home/escritorio-2.jpg') }});">
+            <div class="absolute inset-0 bg-black bg-opacity-30"></div>
+            <div class="text-BlancoTerciario w-1/4 z-10 mr-36 h-screen flex flex-col">
+                <div class="flex-grow flex items-center">
+                    <h1 class="text-5xl">La naturaleza se inclina ante su arte.</h1>
+                </div>
+                <p class="text-sm mb-4">ph. Milena Oleszczuk</p>
+            </div>
+        </div>
+
+
+        <!-- Sección de Beneficios -->
+        <div class="py-12 bg-BlancoTerciario">
+            <div class="container mx-auto grid grid-cols-1 md:grid-cols-3 gap-8 px-4">
+                <!-- Primer Bloque -->
+                <div class="flex flex-col items-center text-center">
+                    <img src="{{ asset('svg/home/Tarjeta.svg') }}" alt="Icono 1" class="w-28 h-28">
+                    <h3 class="mt-4 text-lg w-60 ">HAZTE SOCIO y recibe más beneficios.</h3>
+                    <button class="mt-4 px-2 py-1 bg-[#C7CBC6] text-black hover:bg-[#616261] border-black border-2">
+                        MÁS INFO
+                    </button>
+                </div>
+                <!-- Segundo Bloque -->
+                <div class="flex flex-col items-center text-center">
+                    <img src="{{ asset('svg/home/Megafono.svg') }}" alt="Icono 2" class="w-28 h-28">
+                    <h3 class="mt-4 text-lg w-74 ">NOVEDADES Y ACTUALIDAD,<br> enterate de todo.</h3>
+                    <button class="mt-4 px-2 py-1 bg-[#C7CBC6] text-black hover:bg-[#616261] border-black border-2">
+                        MÁS INFO
+                    </button>
+                </div>
+                <!-- Tercer Bloque -->
+                <div class="flex flex-col items-center text-center">
+                    <img src="{{ asset('svg/home/Trofeo.svg') }}" alt="Icono 3" class="w-28 h-28">
+                    <h3 class="mt-4 text-lg w-60 ">Próximo CONCURSO. <span
+                            class=" bg-MarronSecundario px-2 rounded-full text-white">TOLEDO 2024</span></h3>
+                    <button class="mt-4 px-2 py-1 bg-[#C7CBC6] text-black hover:bg-[#616261] border-black border-2">
+                        MÁS INFO
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+
 </x-layout>
--- resources/views/partials/navigation.blade.php
+++ resources/views/partials/navigation.blade.php
@@ -1,11 +1,33 @@
-<nav class=" flex flex-col">
-    <div class=" container ">
-        <div class=" w-screen flex justify-center h-28 bg-AzulPrimario ">
-            <img src="{{ asset('svg/pointerbanderas.svg') }}" alt="Banderas Navbar" class=" w-1/2 ">
+{{-- <div class=" h-48 bg-black "></div> --}}
+
+<nav class=" fixed top-0 left-0 flex flex-col z-50 ">
+    <div class=" relative ">
+
+        <div class=" w-screen flex justify-center h-20 bg-black bg-opacity-80 ">
+            <img src="{{ asset('svg/navbar/pointerbanderas.svg') }}" alt="Banderas Navbar" class=" w-1/2 ">
         </div>
+
+        <div class=" absolute inset-y-0 right-0 flex items-center mr-20 w-52 gap-4 ">
+
+            <a href="">
+                <img src="{{ asset('svg/navbar/Instagram.svg') }}" alt="Banderas Navbar" class=" w-8 ">
+            </a>
+            <a href="">
+                <img src="{{ asset('svg/navbar/Facebook.svg') }}" alt="Banderas Navbar" class=" w-8 ">
+            </a>
+            <a href="">
+                <img src="{{ asset('svg/navbar/Usuario.svg') }}" alt="Banderas Navbar" class=" w-8 ">
+            </a>
+            <a href="">
+                <img src="{{ asset('svg/navbar/Buscar.svg') }}" alt="Banderas Navbar" class=" w-8 ">
+            </a>
+        </div>
+
     </div>
-    <div>
-        <ul>
+
+    <div class="w-full flex justify-center bg-AzulPrimario relative h-12">
+        <img src="{{ asset('svg/navbar/Logo.svg') }}" alt="Logo del sitio" class="w-28 absolute left-52 top-1/2 -translate-y-1/2">
+        <ul class="flex items-center gap-4 text-white">
             <li><a href="{{ route('elPointer') }}">El Pointer</a></li>
             <li><a href="{{ route('Club') }}">Club</a></li>
             <li><a href="{{ route('Concursos') }}">Concursos</a></li>
@@ -16,4 +38,5 @@
             <li><a href="{{ route('Contacto') }}">Contacto</a></li>
         </ul>
     </div>
+    
 </nav>
