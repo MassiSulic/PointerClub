a028d31637e5195cba9cca17b7ef5bd81cf77415
MassiSulic <massi.sulic@gmail.com>
2024-12-10 11:42:29 -0300

    Vistas del Club terminadas y avances con la vista contacto

--- resources/views/Criaderos.blade.php
+++ resources/views/Criaderos.blade.php
@@ -1,5 +1,43 @@
+@php
+    $headers = ['ASIJO', 'TITULAR', 'PROVINCIA', 'TEL.'];
+    $rows = [
+        ['DEL Estilo Y PASIÓN', 'Adrián Pérez Camacho', 'Valencia', '661944890'],
+        ['DE CASTRONUEVO', 'Alfredo Fernández Zamora', 'Valladolid', '692208112'],
+        ['DES FEUX DE LA PASSION', 'Angel Mercado Puerto', 'Zaragoza', '638050650'],
+        ['DE Q\'SAR BERMEJA', 'Antonio F. Pardo Jurado', 'Malaga', '616746736'],
+        ['DE PASARÍN', 'Antonio González Álvarez', 'Pontevedra', '650485673'],
+        ['MAISALAND', 'Carlos Guitiérrez Martínez', 'Burgos', '630989197'],
+        ['DE TORRES DA VARELA', 'Carlos Lalin Ferreiroa', 'Pontevedra', '659951396'],
+        ['DA BRISA', 'Eliseo Faraldo Fernández', 'A Coruña', '609828746'],
+        ['DEL CERCANO', 'Eugenio Cervantes Cano', 'Ciudad Real', '633645008'],
+        ['DEL SOL DE NAVELINA', 'Francisco García Vicent', 'Valencia', '607714155'],
+        ['DES CRISTO DEL VALLE', 'Francisco Palacios Olmedo', 'Toledo', '609236128'],
+        ['DE URSAONESOL', 'Francisco Sánchez Ropero', 'Sevilla', '609576225'],
+        ['DE AKELARRE', 'Gabriel Ayesta Ortúzar', 'Bizkaia', '659679439'],
+        ['DES DIEUX DU VENT', 'Héctor Trullén del Campo', 'Zaragoza', '695639150'],
+        ['DE GARCILLEJA', 'Iván García Calleja', 'Zaragoza', '686718923'],
+        ['DE LA CAMPIÑUELA', 'Javier Bermúdez Chito', 'Malaga', '686168325'],
+        ['DEBORRAS', 'Jesús Arnal López', 'Castellón', '654655124'],
+        ['DEL TRAPIO', 'José Antonio Martinez del Viso', 'Toledo', '686386488'],
+        ['D\'U PUIG', 'José Ganals Rebassa', 'Baleares', '630057946'],
+        ['DE LA MAZORRA', 'José Condado González', 'Burgos', '619738006'],
+        ['DE MIRANDAOLA', 'José Luis Aragón Diez', 'Zaragoza', '655679112'],
+        ['DEL MANGUERIN', 'José Miguel Redruello Suárez', 'Asturias', '618620799'],
+        ['DE LA PINEDA VELLA', 'Josep Pares Rosinol', 'Barcelona', '686471095'],
+        ['DE OBARALES', 'Juan Miguel Palomo Roldán', 'Cadiz', '625595491'],
+        ['DE LOS DESMONTES', 'Juaquín Bueno Bonilla', 'Jaén', '670668730'],
+        ['DE LA CABRERA Y PUENTE', 'Luis Antonio Álvarez García', 'Leon', '660959985'],
+        ['DE LA PLAZA DE LA MARE', 'Manuel Teijeira Veiga', 'Barcelona', '636234765'],
+        ['DEL CAMP DE LLIRIA', 'Manuel Faubel Muñoz', 'Valencia', '639675557'],
+        ['DE LA PLANA D\'ALBONS', 'Raúl Borrella Colomer', 'Girona', '620992061'],
+        ['DE CASTROGALIA', 'Rubén Álvarez Candame', 'La Coruna', '659054753'],
+    ];
+@endphp
+
 <x-layout>
-    <div class=" mt-48">
-        <h1>Criaderos</h1>
+    <div class="mt-48">
+        <div class=" overflow-x-auto mb-24 lg:mx-48 sm:mx-20 ">
+            <x-table :headers="$headers" :rows="$rows" />
+        </div>
     </div>
 </x-layout>
--- resources/views/Delegaciones.blade.php
+++ resources/views/Delegaciones.blade.php
@@ -1,5 +1,32 @@
+@php
+    $headers = ['DELEGACIÓN', 'APELLIDOS', 'NOMBRE', 'POBLACIÓN', 'PROVINCIA', 'TEL.'];
+    $rows = [
+        ['Galicia', 'Álvarez Rubiños', 'Francisco', 'Lugo', 'Lugo', '676158348'],
+        ['Valencia y Murcia', 'Chilet Aguilar', 'Jesus', 'Pobla de Vallbona', 'Valencia', '615454984'],
+        ['Extremadura', 'Fuentes Duran', 'Rafael', 'Azuaga', 'Badajoz', '699939241'],
+        ['Aragón', 'García Calleja', 'Iván', 'Zaragoza', 'Zaragoza', '686718923'],
+        ['Cataluña', 'Okkinga', 'Dannielle', 'Lloret de Mar', 'Girona', '679828741'],
+        ['Andalucía', 'Palomo Roldán', 'Juan Miguel', 'Jimena de la Frontera', 'Cádiz', '625595491'],
+        ['Mallorca', 'Serra Crespi', 'Agustín', 'Sa Pobla (Mallorca)', 'Islas Baleares', '666539049'],
+        ['País Vasco', 'Areitio Caritaonandia', 'Juan Luis', 'Garai', 'Bizkaia', '659445966'],
+        ['Castilla La Mancha y Madrid', 'Meneses Paños', 'Julian', 'San Clemente', 'Cuenca', '687552626'],
+    ];
+@endphp
+
 <x-layout>
-    <div class=" mt-48">
-        <h1>Delegaciones</h1>
+    <div class="mt-48 mb-24 flex flex-col space-y-12">
+        <!-- Descripción -->
+        <p class="text-center p-3 bg-[#E8E6D9] text-MarronSecundario">
+            En este apartado podrás encontrar a todas las delegaciones, sus representantes y los datos de<br>
+            contacto de cada uno, para posibles dudas o eventos a realizarse en su área representada.
+        </p>
+
+        <!-- Imagen -->
+        <img class="lg:w-1/3 sm:w-96 w-64 mx-auto" src="{{ asset('svg/club/Recurso 12.svg') }}" alt="Mapa con las delegaciones">
+
+        <!-- Tabla -->
+        <div class="overflow-x-auto lg:mx-48 sm:mx-20">
+            <x-table :headers="$headers" :rows="$rows" />
+        </div>
     </div>
 </x-layout>
