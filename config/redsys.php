11ca10f4b620e96421dbb2c09f4f160890764eb9
Ruddy14 <vaserweb@gmail.com>
2025-01-13 10:09:35 -0300

    Prueba de compra real 8

--- config/redsys.php
+++ config/redsys.php
@@ -1,7 +1,7 @@
 <?php
 return [
     'key'                   => env('REDSYS_KEY','jHlCuo6qfK1Hj8wa/eK8iKLbi/FxjBfy'),
-    'url_notification'      => env('REDSYS_URL_NOTIFICATION','https://sis.redsys.es/sis/realizarPago'),
+    'url_notification'      => env('REDSYS_URL_NOTIFICATION','https://pointerclubespana.es/redsys/notification'),
     'url_ok'                => env('REDSYS_URL_OK','https://pointerclubespana.es/redsys/success'),
     'url_ko'                => env('REDSYS_URL_KO','https://pointerclubespana.es/redsys/failure'),
     'merchantcode'          => env('REDSYS_MERCHANT_CODE','058538802'),
