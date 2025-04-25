1d37d11d832effb611570fd1968eab4a4b777aa7
Massimo Sulic <massisulic@MacBook-Air-de-Massimo.local>
2025-04-25 10:03:15 -0300

    Actualización nuevo concurso home

--- resources/views/Inicio.blade.php
+++ resources/views/Inicio.blade.php
@@ -107,7 +107,7 @@
             <div class="grid grid-cols-1 lg:grid-cols-2 bg-MarronSecundario">
                 <!-- Imagen del perro -->
                 <div class="bg-cover bg-top h-full"
-                    style="background-image: url({{ asset('image/home/Concurso-promo-12-13-abril.jpg') }}); min-height: 300px;">
+                    style="background-image: url({{ asset('image/home/ProximoConcurso/PerroConcurso25_04_25.jpg') }}); min-height: 300px;">
                 </div>
 
                 <!-- Contenido del evento -->
@@ -115,7 +115,7 @@
                     {{-- <h1 class="text-3xl font-bold lg:text-left text-center text-white bg-AzulPrimario p-8 lg:pl-20">
                         Próximo CONCURSO.</h1> --}}
                     <a href="https://pointerclubespana.es/Concursos">
-                        <img src="image/home/ProximoConcurso/Concurso 12 y 13 de abril.jpg" alt="">
+                        <img src="image/home/ProximoConcurso/ProximoConcurso25_04_25.jpg" alt="">
                     </a>
                 </div>
             </div>
